59195e1ef00fe030e93e45b315bd0d288ef91f65
ZhangTong <ztt446810938@gmail.com>
2024-04-12 13:49:22 +0100

    commit6

--- src/test/java/com/example/calcuator/CalculatorTest.java
+++ src/test/java/com/example/calcuator/CalculatorTest.java
@@ -6,17 +6,17 @@ public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
     @Test
-    public void testAdd() {
+    void testAdd() {
         assertEquals(5, calculator.add(2, 3), "2 + 3 should equal 5");
     }
 
     @Test
-    public void testSubtract() {
+    void testSubtract() {
         assertEquals(-1, calculator.subtract(2, 3), "2 - 3 should equal -1");
     }
 
     @Test
-    public void testMultiply() {
+    void testMultiply() {
         assertEquals(6, calculator.multiply(2, 3), "2 * 3 should equal 6");
     }
 }
